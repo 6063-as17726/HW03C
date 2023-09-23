101368907bbd556e7a3753db350450288686e1b0
Ayushi Shah <as17726@nyu.edu>
2023-09-23 19:02:50 -0400

    Attempt 1 - making the points

--- sketch.js
+++ sketch.js
@@ -1,6 +1,17 @@
+let max = 50;
+let diam = 10;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-}
+  background(0);
+  fill("white");
+  randomSeed(100);
+
+  for(x = 0; x<=max ; x+=1){
+    let xpos = random(0, windowWidth);
+    let ypos = random(0, windowHeight);
+    ellipse(xpos,ypos,diam,diam);
+    }
+  }
